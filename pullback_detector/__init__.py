d7e2d90dff19002959c8c7c657d59f31c3129dba
zahidshaikmohammed-cmyk <zahidshaikmohammed@gmail.com>
2026-08-11 19:24:10 +0530

    feat: expose V1 benchmark and V2 qualification engine

--- pullback_detector/__init__.py
+++ pullback_detector/__init__.py
@@ -1,3 +1,7 @@
 """Indian liquid-stock pullback detection engine."""
 
-__version__ = "0.1.0"
+from .detector import PullbackDetector, V1PullbackDetector
+from .healthy_pullback_v2 import HealthyPullbackV2
+
+__version__ = "0.2.0"
+__all__ = ["PullbackDetector", "V1PullbackDetector", "HealthyPullbackV2"]
